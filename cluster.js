dc28829c336c93c8c8decdfff670ea5aa8158937
kaljitism <dwikaljit@gmail.com>
2024-08-06 17:45:45 +0530

    spawned a server on all CPU cores cluster module

--- cluster.js
+++ cluster.js
@@ -1,12 +1,22 @@
 const cluster = require("cluster");
 
-console.log("-----");
-
 if (cluster.isPrimary) {
-    console.log("This is the parent proces`s");
-    cluster.fork();
-   
+    console.log(`This is the parent process with PID ${process.pid}`);
+
+    const coresCount = require("os").availableParallelism();
+
+    for (let i = 0; i < coresCount; i++) {
+        const worker = cluster.fork();
+        console.log(`The paprent process has spawned a new child process with pid ${worker.process.pid}`);
+    }
+
+    cluster.on("exit", (worker, exitCode, signal) => {
+        console.log(`Worker ${worker.process.pid} ${signal || exitCode} died, restarting...`);
+        cluster.fork();
+    });
+
 } else {
-    console.log("This is the child process");
+    require("./server.js");
+     
 }
 
